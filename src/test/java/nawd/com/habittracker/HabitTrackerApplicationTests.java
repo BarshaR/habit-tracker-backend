2087bbada4143bcd829082e501f1e51f5c2ed590
Richard <richard@localhost.localdomain>
2020-01-27 21:47:00 +1100

    Initial commit

---
+++ src/test/java/nawd/com/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package nawd.com.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
